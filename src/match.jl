60b4a9415a5cea85d76b709c180b66b9c8aa3dc4
chengchingwen <adgjl5645@hotmail.com>
2022-07-17 17:18:32 +0800

    add ismatch to meta

--- src/match.jl
+++ src/match.jl
@@ -8,14 +8,20 @@ MatchTokenization(base, patterns) = MatchTokenization(base, map(as_match, patter
 @inline splitting(p::ParentStages, t::MatchTokenization, x::SubSentence) = splitting(p, t.base, Sentence(getvalue(x), getmeta(x)))
 @inline splitting(p::ParentStages, t::MatchTokenization, s::SentenceStage) = MatchSplits(t.patterns, getvalue(s))
 
-@inline wrap(p::ParentStages, t::MatchTokenization, s::SentenceStage, (istoken, x)) = istoken ? Token(x, getmeta(s)) : SubSentence(x, getmeta(s))
+@inline function wrap(p::ParentStages, t::MatchTokenization, s::SentenceStage, (istoken, x))
+    meta = updatemeta(getmeta(s), (ismatch = istoken,))
+    return istoken ? Token(x, meta) : SubSentence(x, meta)
+end
 @inline wrap(p::ParentStages, t::MatchTokenization, s::TokenStages, x) = wrap(p, t.base, s, x)
 @inline wrap(p::ParentStages, t::MatchTokenization, x::TokenStages) = wrap(p, t.base, x)
 
 # calling directly on word should check if any match exists
 splittability(::Nothing, ::MatchTokenization, ::WordStage) = Splittable()
 @inline splitting(::Nothing, t::MatchTokenization, w::WordStage) = MatchSplits(t.patterns, getvalue(w))
-@inline wrap(::Nothing, t::MatchTokenization, w::WordStage, (istoken, x)) = istoken ? Token(x, getmeta(w)) : Word(x, getmeta(w))
+@inline function wrap(::Nothing, t::MatchTokenization, w::WordStage, (istoken, x))
+    meta = updatemeta(getmeta(w), (ismatch = istoken,))
+    return istoken ? Token(x, meta) : Word(x, meta)
+end
 
 # show
 function Base.show(io::IO, t::MatchTokenization)
